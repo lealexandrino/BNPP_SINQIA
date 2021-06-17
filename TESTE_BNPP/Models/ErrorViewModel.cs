f4a935e59649333f51dbee63948b51d42d2461ed
Leandro Alexandrino <le.alexandrin.santos@gmail.com>
2021-06-17 15:56:03 -0300

    Primeiro commit - Inicio do desenovlvimento do teste.

---
+++ TESTE_BNPP/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TESTE_BNPP.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
